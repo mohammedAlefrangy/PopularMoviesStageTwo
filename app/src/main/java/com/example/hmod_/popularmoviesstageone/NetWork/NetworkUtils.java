5e6af9f093a2b86defee1a44917fb9331ebf64e4
mohammedAlefrangy <mohamed.alefrangy@gmail.com>
2018-09-09 22:54:21 +0300

    remove the api_key

--- app/src/main/java/com/example/hmod_/popularmoviesstageone/NetWork/NetworkUtils.java
+++ app/src/main/java/com/example/hmod_/popularmoviesstageone/NetWork/NetworkUtils.java
@@ -34,7 +34,7 @@ public class NetworkUtils {
     private final static String TOP_RATED = "/top_rated";
     private final String API_KEY_PARAM = "api_key";
     //TODO (1) insert your api_key here
-    private final String KEY = "0185beec581fa94a036cc0a6240c5712";
+    private final String KEY = "";
 
     public NetworkUtils() {
     }
